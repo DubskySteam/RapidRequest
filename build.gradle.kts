275e7b4d093fc70e42edb0551178a40cb5886446
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-02-27 03:45:34 +0100

    update:build.gradle added dokka && jvm plugins to build source && javadoc jar files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java")
     id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "2.0.0-Beta4"
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 group = "dev.dubsky"
@@ -17,4 +19,29 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("sourcesJar", Jar::class) {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks.dokkaJavadoc {
+    outputDirectory.set(layout.buildDirectory.dir("dokkaJavadoc"))
+}
+
+tasks.register("javadocJar", Jar::class) {
+    from(tasks.named("dokkaJavadoc"))
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
 }
